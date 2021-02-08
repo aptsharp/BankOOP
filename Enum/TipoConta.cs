0afb3df7b5fe9d968f7a3bd3b825ed6cc39bb707
Afonso -aptsharp <aff-on@hotmail.com>
2021-02-08 17:48:00 -0300

    aula 02

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace BankOOP.Classes
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BankOOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("BankOOP");
         }
     }
 }
